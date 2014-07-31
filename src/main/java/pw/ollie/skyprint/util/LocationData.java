030d88f904badbafbc6e3e766c15905a5a581af2
DziNeIT <o.dzineit@gmail.com>
2014-07-31 20:50:52 +0100

    LocationData JavaDocs

--- src/main/java/pw/ollie/skyprint/util/LocationData.java
+++ src/main/java/pw/ollie/skyprint/util/LocationData.java
@@ -2,19 +2,56 @@ package pw.ollie.skyprint.util;
 
 import org.bukkit.Location;
 
+/**
+ * Contains an x, y and z value and can be constructed from a {@link Location}
+ */
 public final class LocationData {
-	public final int x, y, z;
+	/**
+	 * The x co-ordinate for this location
+	 */
+	public final int x;
+	/**
+	 * The y co-ordinate for this location
+	 */
+	public final int y;
+	/**
+	 * The z-co-ordinate for this location
+	 */
+	public final int z;
 
+	/**
+	 * Constructs a LocationData from raw x, y and z values
+	 * 
+	 * @param x
+	 *            The x co-ordinate for this LocationData
+	 * @param y
+	 *            The y co-ordinate for this LocationData
+	 * @param z
+	 *            The z co-ordinate for this LocationData
+	 */
 	public LocationData(final int x, final int y, final int z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
+	/**
+	 * Constructs a LocationData from the values in a {@link Location}
+	 * 
+	 * @param loc
+	 *            The {@link Location} to get co-ordinates from
+	 */
 	public LocationData(final Location loc) {
 		this(loc.getBlockX(), loc.getBlockY(), loc.getBlockZ());
 	}
 
+	/**
+	 * Constructs a new LocationData with the same values as the given
+	 * LocationData
+	 * 
+	 * @param data
+	 *            The LocationData object to get co-ordinates from
+	 */
 	public LocationData(final LocationData data) {
 		this(data.x, data.y, data.z);
 	}
