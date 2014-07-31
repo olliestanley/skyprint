5d178c5abb7fc834dbd6e746c531ecb2113ab2dd
DziNeIT <o.dzineit@gmail.com>
2014-07-31 20:30:01 +0100

    BlockChange JavaDocs

--- src/main/java/pw/ollie/skyprint/util/BlockChange.java
+++ src/main/java/pw/ollie/skyprint/util/BlockChange.java
@@ -4,11 +4,35 @@ import java.util.UUID;
 
 import org.bukkit.Material;
 
+/**
+ * Represents a change in the type of the block at a specific location
+ */
 public final class BlockChange {
+	/**
+	 * The unique ID of the player who initiated the change
+	 */
 	public final UUID player;
+	/**
+	 * The location at which the change took place
+	 */
 	public final LocationData loc;
-	public final Material before, after;
+	/**
+	 * The material type of the block before the change
+	 */
+	public final Material before;
+	/**
+	 * The material type of the block after the change
+	 */
+	public final Material after;
 
+	/**
+	 * Constructs a new BlockChange
+	 *
+	 * @param player The unique ID of the player who caused the change
+	 * @param loc    The location where the change took place
+	 * @param before The material type of the block before the change
+	 * @param after  The material type of the block after the change
+	 */
 	public BlockChange(final UUID player, final LocationData loc,
 			final Material before, final Material after) {
 		this.player = player;
